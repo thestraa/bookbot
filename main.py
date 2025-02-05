702c074b153c66aef22169ee93f4ba9c39a9cabc
thestraa <thestraax@gmail.com>
2025-02-05 21:05:05 +0100

    Finished with the project.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
     book_path = "books/frankenstain.txt"
+    print(f"--- Begin report of {book_path} ---")
     text = get_book_text(book_path)
     num_words = count_words(text)
+    print(f"{num_words} words found in the document\n")
     num_chars = count_characters(text)
-
+    print_chars = print_characters(num_chars)
+    print("--- End report ---")
+    
 def count_words(text):
     counter = 0
     words = text.split()
@@ -20,6 +24,20 @@ def count_characters(text):
         else:
             character_count[character] = 1 
     return character_count
+
+def sort_on(char_list):
+    return char_list["count"]
+
+def print_characters(num_chars):
+    char_list = []
+    for char in num_chars:
+        if char.isalpha():
+            count = num_chars[char]
+            char_list.append({"character": char, "count": count})         
+    char_list.sort(reverse=True, key=sort_on) 
+
+    for dictionary in char_list:
+        print(f"The '{dictionary["character"]}' character was found {dictionary["count"]} times")
 
 def get_book_text(path):
     with open(path) as f:
