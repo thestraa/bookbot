8d920ff7e7973a9d6bf41c801d9a3aa279cc3ee9
thestraa <thestraax@gmail.com>
2025-02-05 15:24:26 +0100

    Created new function to count each character in provided book

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ def main():
     book_path = "books/frankenstain.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
-    print(f"{num_words} words was found in the document.")
-
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+    num_chars = count_characters(text)
 
 def count_words(text):
     counter = 0
@@ -16,4 +11,18 @@ def count_words(text):
         counter += 1
     return counter
 
+def count_characters(text):
+    character_count = {}
+    lower_text = text.lower()
+    for character in lower_text:
+        if character in character_count:
+            character_count[character] += 1
+        else:
+            character_count[character] = 1 
+    return character_count
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
 main()
